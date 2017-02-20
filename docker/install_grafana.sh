5e0d5410490f3c574e9378bcf3aa2328f4d0b6f8
satoshi <bettercpp@foxmail.com>
2017-02-20 17:00:13 +0800

    update

--- docker/install_grafana.sh
+++ docker/install_grafana.sh
@@ -2,11 +2,15 @@
 set -x
 set -e
 
-apt-get install -y curl apt-transport-https
-
-echo 'deb https://packagecloud.io/grafana/stable/debian/ jessie main' >> /etc/apt/sources.list
-curl https://packagecloud.io/gpg.key | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install grafana
+#apt-get install -y curl apt-transport-https
+#
+#echo 'deb https://packagecloud.io/grafana/stable/debian/ jessie main' >> /etc/apt/sources.list
+#curl https://packagecloud.io/gpg.key | sudo apt-key add -
+#sudo apt-get update
+#sudo apt-get install grafana
+sudo apt-get install -y wget dpkg
+wget https://grafanarel.s3.amazonaws.com/builds/grafana_4.0.2-1481203731_amd64.deb
+sudo apt-get install -y adduser libfontconfig
+sudo dpkg -i grafana_4.0.2-1481203731_amd64.deb
 
 service  grafana-server start
